54774acbbf374e67d08508115f50be0dd0880caf
Francisco Dias <fdias@opera.com>
2026-07-16 13:49:22 +0100

    [fix] Issue with Java target (code in old file)

--- source/DiscordSocial_gml/extensions/GMDiscordSocial/AndroidSource/Java/GMDiscordSocial.java
+++ source/DiscordSocial_gml/extensions/GMDiscordSocial/AndroidSource/Java/GMDiscordSocial.java
@@ -1,6 +1,13 @@
 package ${YYAndroidPackageName};
 import java.lang.String;
 import java.nio.ByteBuffer;
+import android.app.Activity;
 
 public final class GMDiscordSocial extends GMDiscordSocialInternal {
+    
+    public GMDiscordSocial() {
+        Activity activity = RunnerActivity.CurrentActivity;
+        com.discord.socialsdk.DiscordSocialSdkInit.setEngineActivity(activity);
+    }
+
 }
